5bd1b3c50f258550af0647e06eebe2cde7b7480f
Novera Lim <lim.novera88@gmail.com>
2018-02-13 01:34:28 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,4 @@
 describe('kittens', function() {
   it('defines kittens as `var kittens = ["Milo", "Otis", "Garfield"]`', function() {
+    expect(kittens).toEqual(["Milo", "Otis", "Garfield"])
     
