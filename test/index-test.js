40c7a39d454c8e566adb88eb783d07ceabe13b42
Novera Lim <lim.novera88@gmail.com>
2018-02-13 01:41:56 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -36,4 +36,6 @@ describe('appendKitten(name)', function() {
   it('appends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged', function() {
     expect(appendKitten("Broom")).toEqual(["Milo", "Otis", "Garfield", "Broom"])
     expect(kittens).toEqual(["Milo", "Otis", "Garfield"])
-    
+  })
+})
+
